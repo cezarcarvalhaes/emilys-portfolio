1bbb7d617f93f0cebe3bfbf2a74a2d0a15886507
Cezar <cezar@Cezars-Buddy-Mac.local>
2020-09-26 22:31:46 -0400

    testing stuff out.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,77 +1,78 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-relative-images',
-            options: {
-              name: 'uploads',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-copy-linked-files',
-            options: {
-              destinationDir: 'static',
-            },
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
-      options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
-      },
-    }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
-}
+	siteMetadata: {
+		title: 'Gatsby Starter Blog',
+		author: {
+			name: 'Kyle Mathews',
+			summary: 'who lives and works in San Francisco building useful things.',
+		},
+		description: 'A starter blog demonstrating what Gatsby can do.',
+		siteUrl: 'https://gatsby-starter-blog-demo.netlify.app/',
+		social: {
+			twitter: 'kylemathews',
+		},
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/content/blog`,
+				name: 'blog',
+			},
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/content/assets`,
+				name: 'assets',
+			},
+		},
+		{
+			resolve: 'gatsby-transformer-remark',
+			options: {
+				plugins: [
+					{
+						resolve: 'gatsby-remark-images',
+						options: {
+							maxWidth: 630,
+						},
+					},
+					{
+						resolve: 'gatsby-remark-responsive-iframe',
+						options: {
+							wrapperStyle: 'margin-bottom: 1.0725rem',
+						},
+					},
+					'gatsby-remark-prismjs',
+					'gatsby-remark-copy-linked-files',
+					'gatsby-remark-smartypants',
+				],
+			},
+		},
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-plugin-google-analytics',
+			options: {
+				//trackingId: 'ADD YOUR TRACKING ID HERE',
+			},
+		},
+		'gatsby-plugin-feed',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: 'Gatsby Starter Blog',
+				short_name: 'GatsbyJS',
+				start_url: '/',
+				background_color: '#ffffff',
+				theme_color: '#663399',
+				display: 'minimal-ui',
+				icon: 'content/assets/gatsby-icon.png',
+			},
+		},
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-netlify-cms',
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// 'gatsby-plugin-offline',
+	],
+};
